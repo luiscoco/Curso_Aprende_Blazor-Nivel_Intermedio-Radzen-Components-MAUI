f5fc6516f2c2b0b3737d557e6b92a865dad06894
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2024-10-04 12:59:36 +0200

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiBlazorRadzen
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
